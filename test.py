5f359c06d4833ae328abcc95be172e2e8dc048c6
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2024-12-25 00:59:14 +0700

    update

--- test.py
+++ test.py
@@ -1,53 +1,53 @@
-import os
-import unittest
-import json
-from app import app
-from app.models import setup_db, Movie, Actor, db
+# import os
+# import unittest
+# import json
+# from app import app
+# from app.models import setup_db, Movie, Actor, db
 
-class CastingAgencyTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app
-        self.client = app.test_client
+# class CastingAgencyTestCase(unittest.TestCase):
+#     def setUp(self):
+#         self.app = app
+#         self.client = app.test_client
 
-        # Configure the database for testing
-        app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://{}:{}@{}/{}'.format(
-            os.environ.get('DB_USER'),
-            os.environ.get('DB_PASSWORD'),
-            'localhost:5432',
-            os.environ.get('DB_NAME')
-        )
-        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#         # Configure the database for testing
+#         app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://{}:{}@{}/{}'.format(
+#             os.environ.get('DB_USER'),
+#             os.environ.get('DB_PASSWORD'),
+#             'localhost:5432',
+#             os.environ.get('DB_NAME')
+#         )
+#         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-        with app.app_context():
-            db.create_all()
+#         with app.app_context():
+#             db.create_all()
 
-        # Tokens for RBAC testing
-        self.casting_assistant_token = os.environ.get('TOKEN_TEST')
-        self.casting_director_token = 'CASTING_DIRECTOR_TOKEN'
-        self.executive_producer_token = 'EXECUTIVE_PRODUCER_TOKEN'
+#         # Tokens for RBAC testing
+#         self.casting_assistant_token = os.environ.get('TOKEN_TEST')
+#         self.casting_director_token = 'CASTING_DIRECTOR_TOKEN'
+#         self.executive_producer_token = 'EXECUTIVE_PRODUCER_TOKEN'
 
-        self.new_movie = {
-            "title": "Test Movie",
-            "releaseDate": "2024-01-01",
-            "idsActor": []
-        }
+#         self.new_movie = {
+#             "title": "Test Movie",
+#             "releaseDate": "2024-01-01",
+#             "idsActor": []
+#         }
 
-        self.new_actor = {
-            "name": "Test Actor",
-            "age": 30,
-            "gender": "male",
-            "idsMovie": []
-        }
+#         self.new_actor = {
+#             "name": "Test Actor",
+#             "age": 30,
+#             "gender": "male",
+#             "idsMovie": []
+#         }
 
-    def tearDown(self):
-        """Executed after each test"""
-        pass
+#     def tearDown(self):
+#         """Executed after each test"""
+#         pass
 
-    def get_headers(self, token):
-        return {
-            "Authorization": f"Bearer {token}"
-        }
+#     def get_headers(self, token):
+#         return {
+#             "Authorization": f"Bearer {token}"
+#         }
     
-# Run tests
-if __name__ == "__main__":
-    unittest.main()
+# # Run tests
+# if __name__ == "__main__":
+#     unittest.main()
